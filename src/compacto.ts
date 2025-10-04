a1b147fc50ef098bf1a66853a1d807b702997cd3
Manuel Aranda <153520812+ManuAranda04@users.noreply.github.com>
2025-10-04 17:49:50 -0300

    Agregado constructor

--- src/compacto.ts
+++ src/compacto.ts
@@ -1,5 +1,9 @@
+import { ESTADO_VEHICULO } from "./estado-vehiculo";
+import { Tarifa } from "./Tarifa";
 import { Vehiculo } from "./vehiculo";
 
 export default class Compacto extends Vehiculo{
-    
+    constructor(numMatricula: number, estado: ESTADO_VEHICULO, kilometro: number, logicaTarifa: Tarifa){
+        super(numMatricula, kilometro, estado, logicaTarifa);
+    }
 }
