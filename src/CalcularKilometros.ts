aa52735b789e38f049d22807448856b77a2734c1
Aylin-Garin <aylingarin@gmail.com>
2025-10-21 17:57:22 -0300

    calcular kilometros y promedio
    agregue en la parte de promedioKmDiarios que tiene que estar tambien los KmTotales para la cuenta

--- src/CalcularKilometros.ts
+++ src/CalcularKilometros.ts
@@ -1,8 +1,14 @@
 export default class CalcularKilometros{
-    public promedioKmDiarios(dias: number): number{
-        return 0
+    public promedioKmDiarios(kmTotales: number, dias: number): number{
+        if(dias <= 0){
+            return 0;
+        }
+        return kmTotales / dias;
     }
     public calcularKmTotales(kmIniciales: number, kmFinales: number): number{
-        return 0
+        if(kmFinales <= kmIniciales){
+            throw new Error ("Los km finales no pueden ser menores que los iniciales.");
+        }
+        return kmFinales - kmIniciales;
     }
 }
