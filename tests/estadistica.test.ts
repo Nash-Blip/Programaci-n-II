6a85bd59ea01af4f9c0079f6f415a5798d7075d7
Manuel Aranda <153520812+ManuAranda04@users.noreply.github.com>
2025-11-11 20:45:43 -0300

    Modificados tests clase Estadistica

--- tests/estadistica.test.ts
+++ tests/estadistica.test.ts
@@ -1,31 +1,47 @@
 import Estadistica from "../src/estadistica";
-import { mock } from "jest-mock-extended";
 import Vehiculo from "../src/vehiculo";
 import DatosEstadistica from "../src/datosEstadistica";
+
+let vehiculoUno: jest.Mocked<Vehiculo>;
+    let vehiculoDos: jest.Mocked<Vehiculo>;
+    let vehiculoTres: jest.Mocked<Vehiculo>;
+
+    let vehiculoUnoDatos: jest.Mocked<DatosEstadistica>;
+    let vehiculoDosDatos: jest.Mocked<DatosEstadistica>;
+    let vehiculoTresDatos: jest.Mocked<DatosEstadistica>;
+
+    function crearDatosEstadisticaMock(): jest.Mocked<DatosEstadistica> {
+      return {
+        getCantidadDeVecesAlquilado: jest.fn(),
+        calcularRentabilidad: jest.fn(),
+      } as unknown as jest.Mocked<DatosEstadistica>;
+    }
+
+    function crearVehiculoMock(datos: jest.Mocked<DatosEstadistica>): jest.Mocked<Vehiculo> {
+      return {
+        datosEstadistica: datos,
+      } as unknown as jest.Mocked<Vehiculo>;
+    }
 
 describe("Tests Estadística", () =>{
     let estadistica: Estadistica;
 
-    let vehiculoUno = mock<Vehiculo>();
-    let vehiculoDos = mock<Vehiculo>();
-    let vehiculoTres = mock<Vehiculo>();
-
-    let vehiculoUnoDatos = mock<DatosEstadistica>();
-    let vehiculoDosDatos = mock<DatosEstadistica>();
-    let vehiculoTresDatos = mock<DatosEstadistica>();
-
     beforeEach(()=>{
         estadistica = new Estadistica();
 
-        vehiculoUno.datosEstadistica = vehiculoUnoDatos;
-        vehiculoDos.datosEstadistica = vehiculoDosDatos;
-        vehiculoTres.datosEstadistica = vehiculoTresDatos;
+        vehiculoUnoDatos = crearDatosEstadisticaMock();
+        vehiculoDosDatos = crearDatosEstadisticaMock();
+        vehiculoTresDatos = crearDatosEstadisticaMock();
+
+        vehiculoUno = crearVehiculoMock(vehiculoUnoDatos);
+        vehiculoDos = crearVehiculoMock(vehiculoDosDatos);
+        vehiculoTres = crearVehiculoMock(vehiculoTresDatos);
 
         vehiculoUnoDatos.getCantidadDeVecesAlquilado.mockReturnValue(10);
         vehiculoDosDatos.getCantidadDeVecesAlquilado.mockReturnValue(8);
         vehiculoTresDatos.getCantidadDeVecesAlquilado.mockReturnValue(15);
 
-        vehiculoUnoDatos.calcularRentabilidad.mockReturnValue(50)
+        vehiculoUnoDatos.calcularRentabilidad.mockReturnValue(50);
         vehiculoDosDatos.calcularRentabilidad.mockReturnValue(500);
         vehiculoTresDatos.calcularRentabilidad.mockReturnValue(150);
 
