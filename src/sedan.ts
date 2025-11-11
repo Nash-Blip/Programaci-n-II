f082232e6618e45ecd0fc63e95ef385943746842
Manuel Aranda <153520812+ManuAranda04@users.noreply.github.com>
2025-11-11 18:10:50 -0300

    Modificado constructor

--- src/sedan.ts
+++ src/sedan.ts
@@ -1,9 +1,11 @@
 import Vehiculo from "./vehiculo";
 import TarifaSedan from "./TarifaSedan";
+import DatosEstadistica from "./datosEstadistica";
+import DatosMantenimiento from "./datosMantenimiento";
 
 export default class Sedan extends Vehiculo{
     constructor(){
-        super(new TarifaSedan(), new DatosMantenimiento());
+        super(new TarifaSedan(), new DatosMantenimiento(0, new Date(), 0), new DatosEstadistica());
         this.tarifaBase = 50;
     }
 }
--- src/suv.ts
+++ src/suv.ts
@@ -1,9 +1,11 @@
 import Vehiculo from "./vehiculo";
 import TarifaSuv from "./TarifaSuv";
+import DatosMantenimiento from "./datosMantenimiento";
+import DatosEstadistica from "./datosEstadistica";
 
 export default class SUV extends Vehiculo{
     constructor(){
-        super(new TarifaSuv(), new DatosMantenimiento());
+        super(new TarifaSuv(), new DatosMantenimiento(0, new Date(), 0), new DatosEstadistica());
         this.tarifaBase = 80;
     }
 }
