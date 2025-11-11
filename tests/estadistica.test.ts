cbd35ce1c7c736f6ca7b48c65e032cf461ab28d5
Manuel Aranda <153520812+ManuAranda04@users.noreply.github.com>
2025-11-11 18:12:39 -0300

    Creados tests unitarios para la clase Estadistica

---
+++ tests/estadistica.test.ts
@@ -0,0 +1,48 @@
+import Estadistica from "../src/estadistica";
+import { mock } from "jest-mock-extended";
+import Vehiculo from "../src/vehiculo";
+import DatosEstadistica from "../src/datosEstadistica";
+
+describe("Tests Estadística", () =>{
+    let estadistica: Estadistica;
+
+    let vehiculoUno = mock<Vehiculo>();
+    let vehiculoDos = mock<Vehiculo>();
+    let vehiculoTres = mock<Vehiculo>();
+
+    let vehiculoUnoDatos = mock<DatosEstadistica>();
+    let vehiculoDosDatos = mock<DatosEstadistica>();
+    let vehiculoTresDatos = mock<DatosEstadistica>();
+
+    beforeEach(()=>{
+        estadistica = new Estadistica();
+
+        vehiculoUno.datosEstadistica = vehiculoUnoDatos;
+        vehiculoDos.datosEstadistica = vehiculoDosDatos;
+        vehiculoTres.datosEstadistica = vehiculoTresDatos;
+
+        vehiculoUnoDatos.getCantidadDeVecesAlquilado.mockReturnValue(10);
+        vehiculoDosDatos.getCantidadDeVecesAlquilado.mockReturnValue(8);
+        vehiculoTresDatos.getCantidadDeVecesAlquilado.mockReturnValue(15);
+
+        vehiculoUnoDatos.calcularRentabilidad.mockReturnValue(50)
+        vehiculoDosDatos.calcularRentabilidad.mockReturnValue(500);
+        vehiculoTresDatos.calcularRentabilidad.mockReturnValue(150);
+
+        estadistica.agregarVehiculos(1, vehiculoUno);
+        estadistica.agregarVehiculos(2, vehiculoDos);
+        estadistica.agregarVehiculos(3, vehiculoTres);
+    })
+
+    test("El metodo vehiculoMasAlquilado debe devolver el vehiculo más alquilado", () => {
+        expect(estadistica.vehiculoMasAlquilado()).toBe(vehiculoTres);
+    })
+
+    test("El metodo menosRentabilidad debe devolver el vehiculo con menor rentabilidad", () => {
+        expect(estadistica.menosRentabilidad()).toBe(vehiculoUno);
+    })
+
+    test("El metodo mayorRentabilidad debe devolver el vehiculo con mayor rentabilidad", () => {
+        expect(estadistica.mayorRentabilidad()).toBe(vehiculoDos);
+    })
+})
