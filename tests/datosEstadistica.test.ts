299647c1d5bcb5982e92a040687477929e6a8381
LihuenCortiana <lihuen.albores.cortiana@gmail.com>
2025-11-11 02:21:49 -0300

    tests datosEstadistica

--- tests/datosEstadistica.test.ts
+++ tests/datosEstadistica.test.ts
@@ -1,16 +1,61 @@
+import DatosEstadistica from "../src/DatosEstadistica";
+
 describe('tests de la clase datos estadistica', () =>{
     let cantidadDeVecesAlquilado: number;
     let ingresosAlquiler: number;
     let costosMantenimiento: number;
-    let datosEstadistica: DatosEstadistica;
+    let datosEstadistica: DatosEstadistica
 
     beforeEach(() => {
-        cantidadDeVecesAlquilado = 0;
-        ingresosAlquiler = 0;
-        costosMantenimiento = 0;
+        datosEstadistica = new DatosEstadistica()
+
     })
 
-    test('test clas aumentarCantidadDeVecesAlquilada', () =>{
-        
+    test('test metodo aumentarCantidadDeVecesAlquilada()', () =>{
+        datosEstadistica.aumentarCantidadDeVecesAlquilado();
+        const resultado = datosEstadistica.getCantidadDeVecesAlquilado();
+        expect(resultado).toBe(1)
+    })
+    test('test metodo getCantidadDeVecesAlquilado()', () =>{
+        const resultado = datosEstadistica.getCantidadDeVecesAlquilado();
+
+        expect(typeof resultado).toBe('number')
+        expect(resultado).toBe(0)
+    })
+
+    test('test metodo aumentarIngresosAlquiler()', () =>{
+        datosEstadistica.aumentarIngresosAlquiler(500);
+        const resultado = datosEstadistica.getIngresosAlquiler();
+
+        expect(resultado).toBe(500)
+    })
+
+    test('test metodo getIngresosAlquiler', () =>{
+        const resultado = datosEstadistica.getIngresosAlquiler();
+
+        expect(typeof resultado).toBe('number')
+        expect(resultado).toBe(0)
+    })
+
+    test('test metodo aumentarCostosMantenimiento()', () =>{
+        datosEstadistica.aumentarCostosMantenimiento(1500);
+        const resultado = datosEstadistica.getCostosMantenimiento()
+
+        expect(resultado).toBe(1500)
+    })
+
+    test('test metodo getCostosMantenimiento()', () =>{
+        const resultado = datosEstadistica.getCostosMantenimiento();
+
+        expect(typeof resultado).toBe('number')
+        expect(resultado).toBe(0)
+    })
+
+    test('test metodo calcularRentabilidad()', () =>{
+        datosEstadistica.aumentarIngresosAlquiler(1000);
+        datosEstadistica.aumentarCostosMantenimiento(350)
+        const resultado = datosEstadistica.calcularRentabilidad();
+
+        expect(resultado).toBe(650)
     })
 })
